084909bd9f10a935572938cc4cdee555e730a538
seohyeonKK <sts05158@gmail.com>
2022-08-15 16:10:14 +0900

    delete variable 'len'

--- GetNextLine2/get_next_line.c
+++ GetNextLine2/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: seohyuki <seohyuki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/13 22:29:17 by seohyuki          #+#    #+#             */
-/*   Updated: 2022/08/15 16:08:02 by seohyuki         ###   ########.fr       */
+/*   Updated: 2022/08/15 16:09:28 by seohyuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,12 @@
 char	*get_line(int fd, char **str, unsigned int *idx)
 {
 	char	*line;
-	size_t	len;
 	char	buf[BUFFER_SIZE + 1];
 	char	*old;
 	int		byte;
 
 	*idx = 0;
-	if (!*str)
-		return (0);
-	len = ft_strlen(*str);
-	if (len == 0)
+	if (!*str || !ft_strlen(*str))
 		return (0);
 	byte = 1;
 	while (!ft_strchr(*str, '\n') && byte)
@@ -38,7 +34,6 @@ char	*get_line(int fd, char **str, unsigned int *idx)
 	while ((*str)[*idx] != '\n' && (*str)[*idx] != '\0')
 		(*idx)++;
 	line = (char *)malloc(sizeof(char) * ((*idx) + 2));
-	// line = NULL;
 	if (!line)
 		return (0);
 	ft_memmove(line, *str, (*idx) + 1);
@@ -76,17 +71,3 @@ char	*get_next_line(int fd)
 	str = update_string(str, idx);
 	return (line);
 }
-
-// #include <fcntl.h>
-
-// int main(void)
-// {
-// 	int fd = open("./only_nl.txt", O_RDONLY);
-// 	while (1)
-// 	{
-// 		char *str = get_next_line(fd);
-// 		if (str == NULL) break;
-// 		printf("%s..", str);
-// 	}
-// 	return (0);
-// }
