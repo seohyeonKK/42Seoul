142824d10546a4ea0cf92709a67db3513a5f53a0
seohyeonKK <sts05158@gmail.com>
2022-09-10 22:02:10 +0900

    create Makefile and remove Libft

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,95 +6,125 @@
 /*   By: seohyuki <seohyuki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/06 17:58:11 by seohyuki          #+#    #+#             */
-/*   Updated: 2022/09/09 21:12:05 by seohyuki         ###   ########.fr       */
+/*   Updated: 2022/09/10 21:49:09 by seohyuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
 #include <unistd.h>
-#include <stdio.h>
 
-void	ft_print_char(char c)
+int	ft_print_char(char c)
 {
 	write(1, &c, 1);
+	return (1);
 }
 
-void	ft_putnbr_fd(int n)
+int	ft_putnbr_fd(int n)
 {
+	int	length;
+
+	length = 0;
 	if (n / 10 != 0)
-		ft_putnbr_fd(n / 10);
+		length += ft_putnbr_fd(n / 10);
 	if (n / 10 == 0 && n < 0)
-		ft_print_char('-');
+		length += ft_print_char('-');
 	n = n % 10;
 	if (n < 0)
 		n *= (-1);
-	ft_print_char(n + '0');
+	length += ft_print_char(n + '0');
+	return (length);
 }
 
-void	ft_putnbr_unsigned_fd(unsigned int n)
+int	ft_putnbr_unsigned_fd(unsigned int n)
 {
+	int	length;
+
+	length = 0;
 	if (n / 10 != 0)
-		ft_putnbr_fd(n / 10);
+		length += ft_putnbr_fd(n / 10);
 	if (n / 10 == 0 && n < 0)
-		ft_print_char('-');
+		length += ft_print_char('-');
 	n = n % 10;
 	if (n < 0)
 		n *= (-1);
-	ft_print_char(n + '0');
+	length += ft_print_char(n + '0');
+	return (length);
 }
 
-void	ft_putnbr_hexadecimal_fd(unsigned int n, char format)
+int	ft_putnbr_hexadecimal_fd(unsigned long n, char format)
 {
+	int	length;
+
+	length = 0;
 	if (n / 16 != 0)
-		ft_putnbr_hexadecimal_fd(n / 16, format);
+		length += ft_putnbr_hexadecimal_fd(n / 16, format);
 	n = n % 16;
 	if (n <= 9)
-		ft_print_char(n + '0');
+		length += ft_print_char(n + '0');
 	else
 	{
 		if (format == 'x')
-			ft_print_char(n - 10 + 'a');
+			length += ft_print_char(n - 10 + 'a');
 		else
-			ft_print_char(n - 10 + 'A');
+			length += ft_print_char(n - 10 + 'A');
 	}
+	return (length);
 }
 
-void	ft_print_string(char *str)
+int	ft_print_string(char *str)
 {
 	int	i;
+
 	i = 0;
+	if (!str)
+	{
+		write(1, "(null)", 6);
+		return (6);
+	}
 	while (str[i])
 	{
 		write(1, str + i, 1);
 		i++;
 	}
+	return (i);
 }
 
-void	ft_print_int(int n)
+int	ft_print_int(int n)
 {
-	ft_putnbr_fd(n);
+	int	length;
+
+	length = ft_putnbr_fd(n);
+	return (length);
 }
 
-void	ft_print_unsigned(unsigned int n)
+int	ft_print_unsigned(unsigned int n)
 {
-	ft_putnbr_unsigned_fd(n);
+	int	length;
+
+	length = ft_putnbr_unsigned_fd(n);
+	return (length);
 }
 
-void	ft_print_hexadecimal(unsigned int n, char format)
+int	ft_print_hexadecimal(unsigned int n, char format)
 {
-	ft_putnbr_hexadecimal_fd(n, format);
+	int	length;
+
+	length = ft_putnbr_hexadecimal_fd(n, format);
+	return (length);
 }
 
-void	ft_print_pointer(unsigned int ptr)
+int	ft_print_pointer(unsigned long ptr)
 {
+	int	length;
+
+	length = 0;
 	write(1, "0x", 2);
+	length += 2;
 	if (ptr == 0)
-		write(1, "0", 1);
+		length += ft_print_char('0');
 	else
-	{
-		write(1, "1", 1);
-		ft_putnbr_hexadecimal_fd(ptr, 'x');
-	}
+		length += ft_putnbr_hexadecimal_fd(ptr, 'x');
+	return (length);
 }
 
 int	ft_printf(const	char *str, ...)
@@ -110,37 +140,23 @@ int	ft_printf(const	char *str, ...)
 		{
 			str++;
 			if (*str == 'c')
-				ft_print_char(va_arg(ap, int));
+				length += ft_print_char(va_arg(ap, int));
 			else if (*str == 's')
-				ft_print_string(va_arg(ap, char *));
+				length += ft_print_string(va_arg(ap, char *));
 			else if (*str == 'd' || *str == 'i')
-				ft_print_int(va_arg(ap, int));
+				length += ft_print_int(va_arg(ap, int));
 			else if (*str == 'u')
-				ft_print_unsigned(va_arg(ap, unsigned int));
+				length += ft_print_unsigned(va_arg(ap, unsigned int));
 			else if (*str == 'x' || *str == 'X')
-				ft_print_hexadecimal(va_arg(ap, unsigned int), *str);
+				length += ft_print_hexadecimal(va_arg(ap, unsigned int), *str);
 			else if (*str == 'p')
-				ft_print_pointer(va_arg(ap, unsigned int));
+				length += ft_print_pointer(va_arg(ap, unsigned long));
 			else if (*str == '%')
-				ft_print_char('%');
+				length += ft_print_char('%');
 		}
 		else
-			ft_print_char(*str);
+			length += ft_print_char(*str);
 		str++;
 	}
-
-	return (0);
-}
-
-int main(void)
-{
-	char str = 'a';
-	int num = 123;
-	// printf("%c");
-	// printf("%%\nhello world!\n", -2147483649);
-	// ft_printf("%%\nhello world!\n", -2147483649);
-	printf("%p\n", &num);
-	ft_printf("%p", &num);
-
-	// printf("%s", NULL);
+	return (length);
 }
--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -6,7 +6,26 @@
 /*   By: seohyuki <seohyuki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/06 18:00:52 by seohyuki          #+#    #+#             */
-/*   Updated: 2022/09/06 18:00:53 by seohyuki         ###   ########.fr       */
+/*   Updated: 2022/09/10 22:00:22 by seohyuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+# include <stdio.h>
+
+int	ft_print_char(char c);
+int	ft_putnbr_fd(int n);
+int	ft_putnbr_unsigned_fd(unsigned int n);
+int	ft_putnbr_hexadecimal_fd(unsigned int n, char format);
+int	ft_print_string(char *str);
+int	ft_print_int(int n);
+int	ft_print_unsigned(unsigned int n);
+int	ft_print_hexadecimal(unsigned int n, char format);
+int	ft_print_pointer(unsigned int ptr);
+int	ft_printf(const	char *str, ...);
+
+#endif
